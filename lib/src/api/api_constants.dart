702dee3b14729e6ad2e9fc1b01e5470f7fd5b295
umangbhatt <umang.bhatt2000@gmail.com>
2022-05-18 17:16:26 +0530

    minor changes

--- lib/src/api/api_constants.dart
+++ lib/src/api/api_constants.dart
@@ -2,6 +2,7 @@ class ApiConstants {
   static const String newsBaseUrl = 'https://newsapi.org/v2';
 
   static const String newApiKey = '2375f3c78a9449949d1b2ded481f3aae';
+  //4f246f76ee2a4ab085d9762f83507ad5
 
   static const String weatherURL = 'https://api.openweathermap.org/data/2.5/weather';
   static const String weatherApiKey = '6e330cafbe96634f3825ce56c4bd99e1';
